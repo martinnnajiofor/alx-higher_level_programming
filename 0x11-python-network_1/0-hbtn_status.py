0f1201947cc12beacc40c1682553a6dabf34105b
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-10-25 04:27:14 -0700

    Python Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,8 @@
     This script fetches https://intranet.hbtn.io/status
 """
 import urllib.request
+
+if __name__ == "__main__":
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
 
