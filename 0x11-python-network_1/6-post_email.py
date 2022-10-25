2f7873aeffe0ef520024a58804f34d79da388265
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-10-25 05:59:49 -0700

    Python Network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter and finally displays the body of the response
 """
-from sys import argv
 import requests
+from sys import argv
+
 
 if __name__ == "__main__":
     para = {'email': sys.argv[2]}
