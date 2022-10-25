da0a38e3827db0d0412d0b59a46aebc399ce0116
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-10-25 04:16:04 -0700

    Python Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,3 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         print('Body response:\n\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode('utf-8')))
-
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,9 +2,14 @@
 """
    This uses the package requests to fetch https://intranet.hbtn.io/status
 """
-import requests
-if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status').text
-    print("Body response:")
-    print("\t- type: {}".format(type(r)))
-    print("\t- content: {}".format(r))
+import urllib.request as request
+import urllib.parse as parse
+from urllib import error
+from sys import argv
+
+if __name__ == '__main__':
+        try:
+            with request.urlopen(argv[1]) as r:
+                print(r.read().decode('utf-8'))
+        except error.HTTPError as e:
+             print("Error code: {}".format(e.code))
