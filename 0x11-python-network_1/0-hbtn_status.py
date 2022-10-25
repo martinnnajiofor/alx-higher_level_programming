f2943447aab0056a9091d7d889d49fea377845af
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-10-25 05:16:55 -0700

    Python Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""This fetches https://intranet.hbtn.io/status."""
+"""This script fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-            html = response.read()
-
-            print('Body response:\n\t- type: {}'.format(type(html)))
-            print('\t- content: {}'.format(html))
-            print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+if __name__ == '__main__':
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf8')}")
