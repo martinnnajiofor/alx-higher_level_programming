4fd70ac8175fd6b7566dccb10456687ece51e9b8
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-10-25 05:47:21 -0700

    Python Network #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""This takes in a URL, sends a rquest to the URL and displays the value of the variable
-X-Request-Id it the response header
-"""
-if __name__ == "__main__":
-    import requests
-    from sys import argv
+"""This script displays the X-Request-Id value"""
+import requests
+from sys import argv
 
-    req = requests.get(sys.argv[1])
+if __name__ == '__main__':
+    req = requests.get(argv[1])
     html = req.headers
     print(html.get('X-Request-Id'))
