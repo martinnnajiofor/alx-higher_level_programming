6e6439cb85da351c793b4ce222bc9555a720315d
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-10-25 00:55:22 -0700

    Python Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""finding the peak module"""
+"""finding the peak"""
 
 
 def find_peak(list_of_integers):
-    """find the peak in a list"""
-    if len(list_of_integers) == 0:
-        return
-    return max(list_of_integers)
+    """find the peak """
+    if list_of_integers:
+        list_of_integers.sort()
+        return (list_of_integers[-1])
+    else:
+        return None
