908e3fa1354ecac38f23b269d8c2cdb2f76f7e20
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-10-25 06:21:20 -0700

    Python Network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    value = {'email': argv[2]}
-    data = requests.post(argv[1], value) 
-    print(data.text)
+    parameter = {'email': argv[2]}
+    body = requests.post(argv[1], parameter) 
+    print(body.text)
