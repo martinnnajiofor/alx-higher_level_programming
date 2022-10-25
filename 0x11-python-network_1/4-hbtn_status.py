be3b95a90c625f88deb5710152d231597eae8311
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-10-25 05:36:39 -0700

    Python Network #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-   This uses the package requests to fetch https://intranet.hbtn.io/status
-"""
+"""This script fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
 if __name__ == '__main__':
-        resp = requests.get('https://alx-intranet.hbtn.io/status')
-        print("Body response:\n\t- type: {}\n\t- content: {}"
-            .format(type(resp.text), resp.text))
+    resp = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(resp.text)}")
+    print(f"\t- content: {resp.text}")
